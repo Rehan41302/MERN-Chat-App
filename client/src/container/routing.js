32f9068a51b504208a65b99b6ea13cc66785c708
GrowthMates <ggrowthmates.com@gmail.com>
2020-03-18 18:02:56 +0500

    v1.0.3

--- client/src/container/routing.js
+++ client/src/container/routing.js
@@ -15,7 +15,7 @@ import Login from './login'
 import Dashboard from '../component/user/dashboard.js'
 import NoMatch from './not-found.js'  
 import socketIOClient from "socket.io-client"
-export const socket = socketIOClient("http://localhost:5000");//secret-brook-13268.herokuapp.com
+export const socket = socketIOClient("http://secret-brook-13268.herokuapp.com");//secret-brook-13268.herokuapp.com //localhost:5000
 // import AllImages from './AllImages'
 
 // Check for token to keep user logged in
